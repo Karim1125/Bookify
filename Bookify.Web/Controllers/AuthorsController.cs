e17ece297b8f7243d05f7d9b3e190573edacceeb
Karim1125 <kareem2468mahmoud@gmail.com>
2025-08-04 06:51:12 +0300

    Add project files.

---
+++ Bookify.Web/Controllers/AuthorsController.cs
@@ -0,0 +1,108 @@
+﻿namespace Bookify.Web.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var authors = _context.Authors.AsNoTracking().ToList();
+
+            var viewModel = _mapper.Map<IEnumerable<AuthorViewModel>>(authors);
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        [AjaxOnly]
+        public IActionResult Create()
+        {
+            return PartialView("_AuthorForm");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(AuthorFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var author = _mapper.Map<Author>(model);
+            _context.Add(author);
+            _context.SaveChanges();
+
+            var viewModel = _mapper.Map<AuthorViewModel>(author);
+
+            return PartialView("_AuthorRow", viewModel);
+        }
+
+        [HttpGet]
+        [AjaxOnly]
+        public IActionResult Edit(int id)
+        {
+            var author = _context.Authors.Find(id);
+
+            if (author is null)
+                return NotFound();
+
+            var viewModel = _mapper.Map<AuthorFormViewModel>(author);
+
+            return PartialView("_AuthorForm", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(AuthorFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var author = _context.Authors.Find(model.Id);
+
+            if (author is null)
+                return NotFound();
+
+            author = _mapper.Map(model, author);
+            author.LastUpdatedOn = DateTime.Now;
+
+            _context.SaveChanges();
+
+            var viewModel = _mapper.Map<AuthorViewModel>(author);
+
+            return PartialView("_AuthorRow", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ToggleStatus(int id)
+        {
+            var category = _context.Authors.Find(id);
+
+            if (category is null)
+                return NotFound();
+
+            category.IsDeleted = !category.IsDeleted;
+            category.LastUpdatedOn = DateTime.Now;
+
+            _context.SaveChanges();
+
+            return Ok(category.LastUpdatedOn.ToString());
+        }
+
+        public IActionResult AllowItem(CategoryFormViewModel model)
+        {
+            var author = _context.Authors.SingleOrDefault(c => c.Name == model.Name);
+            var isAllowed = author is null || author.Id.Equals(model.Id);
+
+            return Json(isAllowed);
+        }
+    }
+}
---
+++ Bookify.Web/Core/Consts/Errors.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Web.Core.Consts
+{
+    public static class Errors
+    {
+        public const string MaxLength = "The maximum length for this field is {1} characters.";
+        public const string Duplicated = "{0} with the same name is already exists!";
+    }
+}
